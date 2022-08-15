43889d47749adbcdad1a05eb0bdb49fe97def036
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-08-15 10:16:57 +0530

    Final Changes

--- src/Components/forgot-password.js
+++ src/Components/forgot-password.js
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../Context/AuthContext";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function ForgotPassword() {
   const emailRef = useRef();
